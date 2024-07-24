098e05d8cd3c674625bedf3586de133d97a7bc45
LaShenaButler <joinadidasradio@gmail.com>
2024-07-24 15:08:10 -0400

    just got my functions buttons to all work

---
+++ app.js
@@ -0,0 +1,39 @@
+function clearText() {
+    document.getElementById('typingArea').value = "";
+}
+
+function bold() {
+document.getElementById('typingArea').style.fontWeight = 'bold'
+}
+
+function italic() {
+document.getElementById('typingArea').style.fontStyle = 'italic'
+}
+
+function underline() {
+document.getElementById('typingArea').style.textDecoration = 'underline'
+}
+
+function center() {
+document.getElementById('typingArea').style.textAlign = 'center'
+}
+
+function left() {
+document.getElementById('typingArea').style.textAlign = 'left'
+}
+
+function right() {
+document.getElementById('typingArea').style.textAlign = 'right'
+}
+
+function uppercase() {
+document.getElementById('typingArea').style.textTransform = 'uppercase'
+}
+
+function lowercase() {
+document.getElementById('typingArea').style.textTransform = 'lowercase'
+}
+
+function capitalize() {
+document.getElementById('typingArea').style.textTransform = 'capitalize'
+}
